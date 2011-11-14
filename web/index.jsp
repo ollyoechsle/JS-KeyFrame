a7f0efcee0382702ce239df081b98ffc9ad26d71
Olly Oechsle <owksley@gmail.com>
2011-11-14 21:45:33 +0000

    Added a built-in "menu context" for navigating up and down a list of elements.

--- web/index.jsp
+++ web/index.jsp
@@ -8,25 +8,14 @@
     <script type="text/javascript" src="javascript/util/function.bind.js"></script>
     <script type="text/javascript" src="javascript/util/customevent.js"></script>
     <script type="text/javascript" src="javascript/keyframe.js"></script>
-    <script type="text/javascript" src="javascript/context.js"></script>
+    <script type="text/javascript" src="javascript/context/context.js"></script>
+    <script type="text/javascript" src="javascript/context/menucontext.js"></script>
     <script type="text/javascript" src="javascript/mappings/keyboard.js"></script>
     <script type="text/javascript" src="javascript/init.js"></script>
     <script type="text/javascript">
 
         jQuery(document).ready(function() {
-
-            var index = 0;
-
-            function goUp() {
-                jQuery("li").removeClass("selected").eq(--index).addClass("selected");
-            }
-
-            function goDown() {
-                jQuery("li").removeClass("selected").eq(++index).addClass("selected");
-            }
-
-            KF.push(new Context({"up" : goUp, "down" : goDown}));
-
+            KF.push(new MenuContext(".menu", "li"));
         });
 
     </script>
@@ -35,7 +24,7 @@
 
 <h1>KeyFrame JS Example</h1>
 
-<ul>
+<ul class="menu">
     <li>Menu 1</li>
     <li>Menu 2</li>
     <li>Menu 3</li>
---
+++ web/javascript/context/menucontext.js
@@ -0,0 +1,82 @@
+(function(KeyFrame, jQuery) {
+
+    /**
+     * Represents a context for navigating up and down a set of elements
+     * @param onActivate A callback that is executed when the context activates
+     * @param onDeactivate A callback that is executed when the context is deactivated
+     */
+    function MenuContext(container, elementSelector, onActivate, onDeactivate) {
+        this.superclass.constructor.call(this, this._generateMapping(), onActivate, onDeactivate);
+        this._jContainer = jQuery(container);
+        this._elementSelector = elementSelector;
+    }
+
+    Object.extend(MenuContext, Context);
+
+    /**
+     * A jQuery object containing a bunch of elements.
+     */
+    MenuContext.prototype._jContainer = null;
+
+    /**
+     * A selector to locate individual elements within the container
+     */
+    MenuContext.prototype._elementSelector = null;
+
+    /**
+     * Internal representation of the current selected element index
+     */
+    MenuContext.prototype._index = null;
+
+    /**
+     * Produces a mapping to go up and down through the list of elements
+     */
+    MenuContext.prototype._generateMapping = function() {
+        return {
+            "up": this.goUp.bind(this),
+            "down": this.goDown.bind(this)
+        }
+    };
+
+    /**
+     * Gets the elements belonging to the container
+     */
+    MenuContext.prototype._getElements = function() {
+        return this._jContainer.find(this._elementSelector);
+    };
+
+    /**
+     * Goes up. If nothing is currently selected, then it starts from the bottom.
+     */
+    MenuContext.prototype.goUp = function() {
+        var elements = this._getElements();
+        if (this._index == null) {
+            this._index = elements.length - 1;
+        } else {
+            this._index--;
+            if (this._index < 0) {
+                this._index = elements.length - 1;
+            }
+        }
+        elements.removeClass("selected").eq(this._index).addClass("selected");
+    };
+
+    /**
+     * Goes down. If nothing is currently selected then it starts from the top.
+     */
+    MenuContext.prototype.goDown = function() {
+        var elements = this._getElements();
+        if (this._index == null) {
+            this._index = 0;
+        } else {
+            this._index++;
+            if (this._index > elements.length - 1) {
+                this._index = 0;
+            }
+        }
+        elements.removeClass("selected").eq(this._index).addClass("selected");
+    };
+
+    window.MenuContext = MenuContext;
+
+})(window.KeyFrame, jQuery);
